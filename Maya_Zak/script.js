80a1e442a16d312635fd1d4aebc6b50937ee771f
AdolfoBCH03 <adolfobatun6@gmail.com>
2023-05-31 08:03:28 -0500

    primer repositorio

---
+++ Maya_Zak/script.js
@@ -0,0 +1,12 @@
+document.getElementById("footer").addEventListener("click", function() {
+    var footer = document.getElementById("footer");
+
+    // Verificar si el footer está oculto
+    if (footer.style.bottom === "") {
+      footer.style.transition = "0.5s";
+      footer.style.bottom = "-" + footer.offsetHeight + "px";
+    } else {
+      footer.style.transition = "0.5s";
+      footer.style.bottom = "";
+    }
+  });
